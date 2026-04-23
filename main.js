5960ed5ea538d87d7490977f2f41872a9bc0f24f
namu0s <namratastha2001@gmail.com>
2026-04-23 17:53:05 +0900

    up

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-// const target = document.getElementById("target");
-// target.textContent = "こんにちは!";
-
-const target = document.getElementById("target");
-target.textContent = "こんにちは";
-target.style.color = "red";
-target.style.fontSize = "30px";
-target.style.backgroundColor = "yellow";
+function sayHello() {
+    alert("Hello");
+}
