fc69b63c7c1cce1f62ca21d20aa82eba9f95ffe5
namu0s <namratastha2001@gmail.com>
2026-04-23 17:05:43 +0900

    my first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// const target = document.getElementById("target");
+// target.textContent = "こんにちは!";
+
+const target = document.getElementById("target");
+target.textContent = "こんにちは";
+target.style.color = "red";
+target.style.fontSize = "30px";
+target.style.backgroundColor = "yellow";
